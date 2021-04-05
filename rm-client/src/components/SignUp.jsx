3621ec0bdb03ea31ad1fff3d0db84f42e850ab0b
shubhpriyadarshi <79093889+shubhpriyadarshi@users.noreply.github.com>
2021-04-05 09:25:23 +0530

    Customer Registration Added with Backend

--- rm-client/src/components/SignUp.jsx
+++ rm-client/src/components/SignUp.jsx
@@ -1,10 +1,24 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import '../css/Customer.css';
+import Axios from 'axios';
 
 function SignUp() {
     const [name, setname] = useState("");
     const [password, setpassword] = useState("");
-    const [mail, setemail] = useState("");
+    const [phonenumber, setphonenumber] = useState(0);
+    const [email, setemail] = useState("");
+
+    const addCustomer = () => {
+      Axios.post("http://localhost:3001/create-user", {
+        name: name,
+        password: password,
+        phonenumber: phonenumber,
+        email: email
+      }).then(() => {
+        console.log("Values Inserted");
+      });
+    };
+    
     return (
       <div>
           <form>
@@ -12,13 +26,23 @@ function SignUp() {
               <div className="container">
                 <h2><b>- Details -</b></h2>
                 <br/>
-                <input className="textbox" type="text" name="name" placeholder="Enter Your Full Name" size="35"/>
+                <input onChange={(event)=>{
+                setname(event.target.value);
+                }} className="textbox" type="text" name="name" placeholder="Enter Your Full Name" size="35"/>
                 <br/>
-                <input className="textbox" type="text" name="email" placeholder="Enter Your Email" size="35"/>
+                <input onChange={(event)=>{
+                setpassword(event.target.value);
+                }} className="textbox" type="text" name="password" placeholder="Enter Your Password" size="35"/>
                 <br/>
-                <input className="textbox" type="text" name="moblieno" placeholder="Enter Your Mobile No" size="35"/>
+                <input onChange={(event)=>{
+                setphonenumber(event.target.value);
+                }} className="textbox" type="text" name="phonenumber" placeholder="Enter Your Mobile No" size="35"/>
                 <br/>
-                <a href="Qualifications.html"><input className="btn_Next" type="button" name="next" value="Submit"/></a>
+                <input onChange={(event)=>{
+                setemail(event.target.value);
+                }} className="textbox" type="text" name="email" placeholder="Enter Your Mail" size="35"/>
+                <br/>
+                <a onClick={addCustomer} href=""><input className="btn_Next" type="button" name="next" value="Submit"/></a>
                 <input className="btn_clear" type="reset" name="clear" value="Clear"/>
               </div>
             </div>
--- rm-server/CustomerSignUp.js
+++ rm-server/CustomerSignUp.js
@@ -1,29 +1,38 @@
 const express = require("express"); 
 const mysql = require("mysql");
 const app = express(); 
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.json());
 
 const db= mysql.createConnection({
-  user: 'root',
-  host: 'localhost',
-  password: 'mysql',
-  database: 'dbms',
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "dbms",
 });
 
-  app.post('/create-user',(req, res) => {
-    const username = req.body.username;
-    const userId = req.body.username+"";
-    const email = req.body.email;
-    const password = req.body.username;
+app.post("/create-user", (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const userId = req.body.phonenumber;
+  const password = req.body.password;
+  const email = req.body.email;
+  const phonenumber = req.body.phonenumber;
 
-  db.query("INSERT INTO Customers (CustomerId, Name, Email, Password, Phone Number ) VALUES ('username,userId, email, password);")
-  }),(err, result) => {
-    if(err){
-    console.log(err);
-  } else{
-    res.send("Values Inserted")
+  
+db.query("INSERT INTO customer (CustomerId, Name, Email, Password, PhoneNumber) VALUES (?,?,?,?,?)",
+    [userId, name, email, password, phonenumber],
+    (err, result) => {
+      if(err){
+      console.log(err);
+    } else{
+      res.send("Values Inserted")
+    }
   }
-}
+  );
+});
 
 app.listen(3001, () => {
   console.log("running server"); 
